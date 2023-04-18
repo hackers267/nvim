f8859282bde7d372570e7e348c8fc21e18b16487
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-18 14:44:53 +0800

    chore: 🚧修改lazy插件管理器

---
+++ lua/user/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+  },
+}
---
+++ lua/user/plugins/init.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        -- build = ":MasonUpdate",
+	config = function()
+	 require("mason").setup()
+	end
+    },
+    {
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+	  "neovim/nvim-lspconfig"
+	},
+	config = function()
+	  require("mason-lspconfig").setup()
+	end
+    },
+}
