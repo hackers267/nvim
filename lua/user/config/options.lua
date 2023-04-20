90a006a7c7a00506c4040be3523e101d88effbd8
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-20 16:14:13 +0800

    feat: 添加esc按钮映射

--- lua/user/config/options.lua
+++ lua/user/config/options.lua
@@ -45,7 +45,11 @@ opt.undolevels = 10000
 opt.updatetime = 200 -- Save swap file and trigger CursorHold
 opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
+opt.wrap = true -- Disable line wrap
+
+local api = vim.api
+
+api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
