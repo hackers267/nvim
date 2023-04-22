873d672ab0fa0cf414829a6b6fa0d1b2f4342074
silence_zhpf <silence_zhpf@aliyun.com>
2023-04-22 22:41:29 +0800

    refactor: ♻️ 单独设置lsp模块

--- lua/user/plugins/cmp/init.lua
+++ lua/user/plugins/cmp/init.lua
@@ -53,39 +53,6 @@ return {
       }
     end,
   },
-  { "hrsh7th/cmp-nvim-lsp",
-  dependencies = {
-      "rafamadriz/friendly-snippets",
-      "L3MON4D3/LuaSnip",
-    "neovim/nvim-lspconfig"
-  },
-  config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local lspconfig = require("lspconfig")
-    local servers = {'lua_ls','tsserver','cssls','eslint','rust_analyzer'}
-    for _,lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-        capabilities = capabilities,
-      }
-    end
-    lspconfig.lua_ls.setup {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = {'vim','awesome'},
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-          telemetry = {
-            enable = false,
-          }
-        },
-      }
-    }
-   end,
-   },
   { "hrsh7th/cmp-buffer",
     config = function()
       local cmp = require('cmp')
@@ -98,8 +65,5 @@ return {
     end,
   },
   { "hrsh7th/cmp-nvim-lua" },
-  { "hrsh7th/cmp-nvim-lsp-signature-help" },
-  { "L3MON4D3/LuaSnip" },
-  { "saadparwaiz1/cmp_luasnip" },
   rust,
 }
